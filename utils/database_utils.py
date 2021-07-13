bd7e0b28c24e26bff5d962f511aa3defcc7bf8d4
Rishikesh Gajelli <rishikeshgajelli@gofynd.com>
2021-07-13 17:45:53 +0530

    heroku-test

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -11,7 +11,7 @@ def get_master_engine():
 
     if not master_engine:
         master_engine = create_engine(
-            "postgresql+psycopg2://smallcase_read_write:smallcaseTracker2021@postgresql/smallcase_tracker",
+            "postgresql+psycopg2://cyueqonebgcvcq:739b3a6ef7e44f3f8483add96f25b14c7a62692192e90513ce848926160033c0@ec2-35-168-145-180.compute-1.amazonaws.com:5432/danch95tg7ijgl",
             pool_recycle=3600,
             pool_size=10,
             pool_timeout=1,
